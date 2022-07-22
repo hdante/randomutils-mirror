d74fe267b0a52a756b5504b6656515217ead0e1f
Henrique Dante de Almeida <hdante@gmail.com>
2022-07-22 13:55:07 -0300

    Add example script: random-password.sh
    Generates easy to remember passwords composed of 3 symbols, 3 words
    from the system dictionary and separators.

---
+++ examples/random-password.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+RANDOM_=${RANDOM_PROG:-random}
+NUM_PASSWORDS=${NUM_PASSWORDS:-5}
+WORDS_PER_PASSWORD=${WORDS_PER_PASSWORD:-3}
+WORD_SEPARATOR=${WORD_SEPARATOR:-,}
+DICTIONARY=${DICTIONARY:-"/usr/share/dict/words"}
+
+dictionary_size=$(wc -l $DICTIONARY | cut -d ' ' -f 1)
+
+if test ! -f "$RANDOM_"; then
+	echo Random program not found. Path searched: "$RANDOM_"
+	exit 1
+fi
+
+if test ! -f "$DICTIONARY"; then
+	echo Dictionary \""$DICTIONARY"\" does not exist.
+	echo Please provide a file with a word list in the DICTIONARY\
+             environment variable.
+	exit 1
+fi
+
+i=1
+while [ $i -le $NUM_PASSWORDS ]
+do
+	prefix=$("$RANDOM_" -8 | head -c 3)
+	words=$("$RANDOM_" $WORDS_PER_PASSWORD 1 $dictionary_size |
+		xargs -I@ sed -n "@{p;q}" "$DICTIONARY")
+	echo $prefix:-$(echo $words | tr " " "$WORD_SEPARATOR")
+	i=$((i+1))
+done
+
