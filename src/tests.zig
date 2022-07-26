e5992ae3f3a1ef7e56839667534ab674386af2f7
Henrique Dante de Almeida <hdante@gmail.com>
2022-07-26 10:35:13 -0300

    Add test file

---
+++ src/tests.zig
@@ -0,0 +1,23 @@
+// randomutils: Generate 64-bit random numbers.
+// Copyright © 2022 Henrique Dante de Almeida
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+test {
+        _ = @import("generator.zig");
+        _ = @import("lottery.zig");
+        _ = @import("random.zig");
+        _ = @import("sysdeps.zig");
+        _ = @import("utils.zig");
+}
